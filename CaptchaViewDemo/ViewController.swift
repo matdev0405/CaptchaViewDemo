ba6a40107bdf15da626a64fbe55300d2ac7c2094
matdev0405 <77271966+matdev0405@users.noreply.github.com>
2022-05-13 12:08:53 +0800

    Initial Commit

---
+++ CaptchaViewDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CaptchaViewDemo
+//
+//  Created by User on 2022/5/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
